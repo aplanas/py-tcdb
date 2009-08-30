94cabf8997edb695f1c9b10b61b334aab3437f49
Alberto Planas <aplanas@gmail.com>
2009-08-30 14:43:05 +0200

    First setup.py version.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tcdb',
+      version='0.1',
+      description='Tokyo Cabinet ctypes Python binding.',
+      author='Alberto Planas',
+      author_email='aplanas@gmail.com',
+      url='http://code.google.com/p/py-tcdb/',
+      packages=['tcdb'],
+      )
